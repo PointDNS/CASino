4c7ffa3828b4843d901a58624739d6ee17f1a812
sovaavladislav <sovaavladislav@gmail.com>
2024-01-22 16:31:04 +0400

    replaced removed 'nothing: true' with 'body: nil'

--- spec/controllers/service_tickets_controller_spec.rb
+++ spec/controllers/service_tickets_controller_spec.rb
@@ -6,7 +6,7 @@ describe CASino::ServiceTicketsController do
     it 'calls the process method of the LegacyValidator' do
       CASino::LegacyValidatorProcessor.any_instance.should_receive(:process).with(kind_of(Hash)) do |params|
         params.should == controller.params
-        controller.render nothing: true
+        controller.render body: nil
       end
       get :validate, params
     end
@@ -17,7 +17,7 @@ describe CASino::ServiceTicketsController do
     it 'calls the process method of the LegacyValidator' do
       CASino::ServiceTicketValidatorProcessor.any_instance.should_receive(:process).with(kind_of(Hash)) do |params|
         params.should == controller.params
-        controller.render nothing: true
+        controller.render body: nil
       end
       get :service_validate, params
     end
